86b436ef207542fdb7d4a5831d6e1e0f771c15f1
pasinduweerasekara <wmpmweerasekara@gmail.com>
2024-05-17 22:36:03 +0530

    footer development started

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Footer from './components/footer/footer';
 import Navbar from './components/navbar/navbar'
 import Home from './pages/home';
 
@@ -9,6 +10,7 @@ function App() {
     <>
     <Navbar/>
     <Home/>
+    <Footer/>
     </>
   )
 }
